1b0c103314c044d7b20a3a9d735f365b30659d6c
zovalger <zovalger@gmail.com>
2024-06-11 00:02:29 -0400

    doc req screenshot and ToDo

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,5 +1,89 @@
 import logo_color from "@/assets/logo_color.jpg";
-import screen_example from "@/assets/screen_example.jpg";
+
+import zovalger_todo_1 from "@/assets/ToDo/zovalger_todo_1.png";
+
+import docReq_1 from "@/assets/docReq/docReq_1.png";
+import docReq_2 from "@/assets/docReq/docReq_2.png";
+import docReq_3 from "@/assets/docReq/docReq_3.png";
+import docReq_4 from "@/assets/docReq/docReq_4.png";
+import docReq_5 from "@/assets/docReq/docReq_5.png";
+import docReq_6 from "@/assets/docReq/docReq_6.png";
+import docReq_7 from "@/assets/docReq/docReq_7.png";
+import docReq_8 from "@/assets/docReq/docReq_8.png";
+import docReq_9 from "@/assets/docReq/docReq_9.png";
+import docReq_10 from "@/assets/docReq/docReq_10.png";
+import docReq_11 from "@/assets/docReq/docReq_11.png";
+import docReq_12 from "@/assets/docReq/docReq_12.png";
+import docReq_13 from "@/assets/docReq/docReq_13.png";
+import docReq_14 from "@/assets/docReq/docReq_14.png";
+import docReq_15 from "@/assets/docReq/docReq_15.png";
+import docReq_16 from "@/assets/docReq/docReq_16.png";
+
+import jeanger_1 from "@/assets/jeanger/jeanger_1.jpg";
+import jeanger_2 from "@/assets/jeanger/jeanger_2.jpg";
+import jeanger_3 from "@/assets/jeanger/jeanger_3.jpg";
+import jeanger_4 from "@/assets/jeanger/jeanger_4.jpg";
+import jeanger_5 from "@/assets/jeanger/jeanger_5.jpg";
+
+import fupagua_1 from "@/assets/fupagua/fupagua_1.png";
+import fupagua_2 from "@/assets/fupagua/fupagua_2.png";
+import fupagua_3 from "@/assets/fupagua/fupagua_3.png";
+import fupagua_4 from "@/assets/fupagua/fupagua_4.png";
+import fupagua_5 from "@/assets/fupagua/fupagua_5.png";
+import fupagua_6 from "@/assets/fupagua/fupagua_6.png";
+import fupagua_6_6 from "@/assets/fupagua/fupagua_6_1.png";
+import fupagua_7 from "@/assets/fupagua/fupagua_7.png";
+import fupagua_8 from "@/assets/fupagua/fupagua_8.png";
+import fupagua_9 from "@/assets/fupagua/fupagua_9.png";
+import fupagua_10 from "@/assets/fupagua/fupagua_10.png";
+import fupagua_11 from "@/assets/fupagua/fupagua_11.png";
+import fupagua_12 from "@/assets/fupagua/fupagua_12.png";
+import fupagua_13 from "@/assets/fupagua/fupagua_13.png";
+import fupagua_14 from "@/assets/fupagua/fupagua_14.png";
+import fupagua_15 from "@/assets/fupagua/fupagua_15.png";
+import fupagua_16 from "@/assets/fupagua/fupagua_16.png";
+
+import unerg_tgs_1 from "@/assets/unerg_tgs/unerg_tgs_1.jpg";
+import unerg_tgs_2 from "@/assets/unerg_tgs/unerg_tgs_2.jpg";
+import unerg_tgs_3 from "@/assets/unerg_tgs/unerg_tgs_3.jpg";
+import unerg_tgs_4 from "@/assets/unerg_tgs/unerg_tgs_4.jpg";
+import unerg_tgs_5 from "@/assets/unerg_tgs/unerg_tgs_5.jpg";
+import unerg_tgs_6 from "@/assets/unerg_tgs/unerg_tgs_6.jpg";
+import unerg_tgs_7 from "@/assets/unerg_tgs/unerg_tgs_7.jpg";
+import unerg_tgs_8 from "@/assets/unerg_tgs/unerg_tgs_8.jpg";
+import unerg_tgs_9 from "@/assets/unerg_tgs/unerg_tgs_9.jpg";
+import unerg_tgs_10 from "@/assets/unerg_tgs/unerg_tgs_10.jpg";
+import unerg_tgs_11 from "@/assets/unerg_tgs/unerg_tgs_11.jpg";
+import unerg_tgs_12 from "@/assets/unerg_tgs/unerg_tgs_12.jpg";
+import unerg_tgs_13 from "@/assets/unerg_tgs/unerg_tgs_13.jpg";
+import unerg_tgs_14 from "@/assets/unerg_tgs/unerg_tgs_14.jpg";
+import unerg_tgs_15 from "@/assets/unerg_tgs/unerg_tgs_15.jpg";
+import unerg_tgs_16 from "@/assets/unerg_tgs/unerg_tgs_16.jpg";
+import unerg_tgs_17 from "@/assets/unerg_tgs/unerg_tgs_17.jpg";
+import unerg_tgs_18 from "@/assets/unerg_tgs/unerg_tgs_18.jpg";
+import unerg_tgs_19 from "@/assets/unerg_tgs/unerg_tgs_19.jpg";
+import unerg_tgs_20 from "@/assets/unerg_tgs/unerg_tgs_20.jpg";
+import unerg_tgs_21 from "@/assets/unerg_tgs/unerg_tgs_21.jpg";
+
+import odaly_sport_1 from "@/assets/odaly_sport/odaly_sport_1.jpg";
+import odaly_sport_2 from "@/assets/odaly_sport/odaly_sport_2.jpg";
+import odaly_sport_3 from "@/assets/odaly_sport/odaly_sport_3.jpg";
+import odaly_sport_4 from "@/assets/odaly_sport/odaly_sport_4.jpg";
+import odaly_sport_5 from "@/assets/odaly_sport/odaly_sport_5.jpg";
+import odaly_sport_6 from "@/assets/odaly_sport/odaly_sport_6.jpg";
+import odaly_sport_7 from "@/assets/odaly_sport/odaly_sport_7.jpg";
+import odaly_sport_8 from "@/assets/odaly_sport/odaly_sport_8.jpg";
+import odaly_sport_9 from "@/assets/odaly_sport/odaly_sport_9.jpg";
+import odaly_sport_10 from "@/assets/odaly_sport/odaly_sport_10.jpg";
+import odaly_sport_11 from "@/assets/odaly_sport/odaly_sport_11.jpg";
+
+import gothere_1 from "@/assets/gothere/gothere_1.jpg";
+import gothere_2 from "@/assets/gothere/gothere_2.jpg";
+import gothere_3 from "@/assets/gothere/gothere_3.jpg";
+import gothere_4 from "@/assets/gothere/gothere_4.jpg";
+import gothere_5 from "@/assets/gothere/gothere_5.jpg";
+import gothere_6 from "@/assets/gothere/gothere_6.jpg";
+import gothere_7 from "@/assets/gothere/gothere_7.jpg";
 
 const content = {
 	nav: {
@@ -15,6 +99,85 @@ const content = {
 	},
 
 	proyects: [
+		{
+			title: "Zovalger ToDo",
+
+			description: [
+				"Software full stack inspirada en la aplicación Microsoft ToDo",
+				"Nota: En desarrollo ",
+			],
+			githubLinks: [
+				"https://github.com/zovalger/to_do_app_server",
+				"https://github.com/zovalger/to_do_app",
+			],
+			siteLink: ["https://to-do-app-zovalger.vercel.app/"],
+			technologys: [
+				"React",
+				"Next.Js",
+				"Material Ui",
+				"Redux",
+				"Axios",
+				"Moment",
+				"Formik",
+				"Socket.IO",
+				"Yup",
+				"Express",
+				"JWT",
+				"Mongoose",
+				"MongoDB",
+				"Cloudinary",
+			],
+			imgs: [zovalger_todo_1],
+		},
+
+		{
+			title: "Petición de documentos de la UNERG",
+			description: [
+				"Este proyecto  Full Stack MERN es el producto de mi trabajo especial de grado, el Frontend está desarrollado en next.js y el servidor con node.js y express, además se encuentran en hosting separados, lo cual todas las comunicaciones son realizadas por la API REST del servidor",
+				"Tiene el objetivo de gestionar las peticiones de documentos de pregrado en la UNERG, posee el módulo del usuario estudiante que le permite realizar todas sus peticiones de documentos además del envío del comprobante de pago.",
+				"También están otros módulos para los usuarios administrativos los cuales tienen acceso según sus permisos otorgados, estos módulos son creador de documentos, validación de pagos y el de gestión de peticiones para el marcado de solicitudes como impreso o entregado.",
+			],
+			githubLinks: [
+				"https://github.com/zovalger/unerg_doc_request_app",
+				"https://github.com/zovalger/unerg_doc_request_server",
+			],
+			siteLink: ["https://unerg.vercel.app/"],
+			technologys: [
+				"React",
+				"Next.Js",
+				"Material Ui",
+				"Redux",
+				"Axios",
+				"Moment",
+				"Formik",
+				"Socket.IO",
+				"Yup",
+				"Express",
+				"JWT",
+				"Mongoose",
+				"MongoDB",
+				"Cloudinary",
+			],
+			imgs: [
+				docReq_1,
+				docReq_2,
+				docReq_3,
+				docReq_4,
+				docReq_5,
+				docReq_6,
+				docReq_7,
+				docReq_8,
+				docReq_9,
+				docReq_10,
+				docReq_11,
+				docReq_12,
+				docReq_13,
+				docReq_14,
+				docReq_15,
+				docReq_16,
+			],
+		},
+
 		{
 			title: "Jeanger",
 			description: [
@@ -43,7 +206,152 @@ const content = {
 				"Node-Cron",
 				"MongoDB",
 			],
-			imgs: [screen_example, screen_example],
+			imgs: [jeanger_1, jeanger_2, jeanger_3, jeanger_4, jeanger_5],
+		},
+		{
+			title: "Fupagua",
+			description: [
+				"Es un software Full Stack desarrollado para la fundación FUPAGUA,  cuenta con un módulo de registro de libros, registros de pacientes, y módulo gestión multimedia para su landing page, además de un registro de usuarios para el uso interno del software según los permisos otorgados a dicho usuario",
+			],
+			githubLinks: ["https://github.com/zovalger/fupagua-Internal-system"],
+			siteLink: ["https://fupagua-internal-system-copy.onrender.com/"],
+			technologys: [
+				"Axios",
+				"Bootstrap",
+				"Formik",
+				"Moment.js",
+				"Reactstrap",
+				"Yup",
+				"Sass",
+				"Cloudinary",
+				"Express",
+				"JWT",
+				"Sequelize",
+				"MySQL",
+				"Jimp",
+			],
+			imgs: [
+				fupagua_1,
+				fupagua_2,
+				fupagua_3,
+				fupagua_4,
+				fupagua_5,
+				fupagua_6,
+				fupagua_6_6,
+				fupagua_7,
+				fupagua_8,
+				fupagua_9,
+				fupagua_10,
+				fupagua_11,
+				fupagua_12,
+				fupagua_13,
+				fupagua_14,
+				fupagua_15,
+				fupagua_16,
+			],
+		},
+
+		{
+			title: "UNERG-TGS",
+			description: [
+				"Sistema de información para el control, monitoreo y localización por geolocalización para los buses de las rutas del transporte universitario de la UNERG. El proyecto consiste en un sistema de información que facilite información primeramente a los estudiantes sobre las rutas de transporte de la UNERG junto con sus detalles de paradas y horarios establecidos.",
+				"Al mismo tiempo otorgarles a los encargados de la administración, una herramienta que les permita monitorear la actividad de los autobuses y a su vez facilitar la asignación de rutas y unidades de transporte a los choferes disponibles.",
+			],
+			githubLinks: ["https://github.com/zovalger/unerg_tgs"],
+			siteLink: ["https://unerg-tgs.onrender.com/"],
+			technologys: [
+				"Next.js",
+				"Nodemailer",
+				"Axios",
+				"Bootstrap",
+				"Cloudinary",
+				"Formik",
+				"Jose",
+				"JWT",
+				"Leaflet",
+				"Leaflet-Routing-Machine",
+				"Moment",
+				"Mongoose",
+				"React",
+				"Socket",
+				"Reactstrap",
+				"Yup",
+			],
+			imgs: [
+				unerg_tgs_1,
+				unerg_tgs_2,
+				unerg_tgs_3,
+				unerg_tgs_4,
+				unerg_tgs_5,
+				unerg_tgs_6,
+				unerg_tgs_7,
+				unerg_tgs_8,
+				unerg_tgs_9,
+				unerg_tgs_10,
+				unerg_tgs_11,
+				unerg_tgs_12,
+				unerg_tgs_13,
+				unerg_tgs_14,
+				unerg_tgs_15,
+				unerg_tgs_16,
+				unerg_tgs_17,
+				unerg_tgs_18,
+				unerg_tgs_19,
+				unerg_tgs_20,
+				unerg_tgs_21,
+			],
+		},
+
+		{
+			title: "Odaly Sport",
+			description: [
+				"Es un e-commerce ficticio para la venta de prendas deportivas, posee vistas con acceso limitado para los usuarios no registrados y registro para usuarios, registro de compras para los usuarios, método de pago por tarjeta de crédito (se usó una api ficticia para validar el pago ). Además cuenta con una interfaz administrativa que posee CRUD de categorías, registro de transacciones, registro de clientes, CRUD de productos a los que se le tiene la posibilidad de colocar links de imágenes y colocar una como destacada.",
+			],
+			githubLinks: ["https://github.com/odalypadrino/P3_16804490"],
+			siteLink: ["https://sport-shop-odaly.onrender.com/"],
+			technologys: [
+				"Node.js",
+				"EJS",
+				"Passport",
+				"Cookies",
+				"Sequelize",
+				"Sqlite",
+				"Typescript",
+				"JWT",
+				"Sendgrid",
+			],
+			imgs: [
+				odaly_sport_1,
+				odaly_sport_2,
+				odaly_sport_3,
+				odaly_sport_4,
+				odaly_sport_5,
+				odaly_sport_6,
+				odaly_sport_7,
+				odaly_sport_8,
+				odaly_sport_9,
+				odaly_sport_10,
+				odaly_sport_11,
+			],
+		},
+
+		{
+			title: "Gothere ",
+			description: [
+				"Es una landing page destinada a una empresa ficticia sobre turismo dentro del estado Guárico de Venezuela, expresa todos los servicios y objetivos de dicha empresa",
+			],
+			githubLinks: ["https://github.com/zovalger/gothere"],
+			siteLink: ["https://gothere-site.vercel.app/"],
+			technologys: ["React", "Next.JS", "Bootstrap", "React-Bootstrap"],
+			imgs: [
+				gothere_1,
+				gothere_2,
+				gothere_3,
+				gothere_4,
+				gothere_5,
+				gothere_6,
+				gothere_7,
+			],
 		},
 	],
 
